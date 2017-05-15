9687b0ce25600d66b45a10d94c404db0c018155b
Francisco Trucco <franciscoctrucco@gmail.com>
2017-05-15 03:10:06 -0300

    Add requirements and setup files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='randoms',
+      version='0.1',
+      description='Random number Generators',
+      license="GPLv3",
+      author='Francisco C. Trucco <franciscoctrucco@gmail.com>,'
+             'Juan M. Scavuzzo <juansca1229@gmail.com>',
+      packages=find_packages())
