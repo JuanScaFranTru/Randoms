69dda5af74f0070086cb71b6f0522f110dc1e593
Francisco Trucco <franciscoctrucco@gmail.com>
2017-05-15 22:52:32 -0300

    Add plot curve to tools

--- tools/tools.py
+++ tools/tools.py
@@ -4,12 +4,19 @@ from numpy import var
 import discretes.distributions as dd
 import matplotlib.pyplot as plt
 from scipy.stats import iqr
+import numpy as np
 
 
 def print_stats(xs):
     """Print mean and variance of xs in stdout."""
     template = "Mean: {:2.2f} \t Variance: {:2.2f}"
     print(template.format(mean(xs), var(xs)))
+
+
+def plotcurve(f, n, a, b):
+    xs = np.arange(a, b, (b-a)/n)
+    ys = [f(x) for x in xs]
+    plt.plot(xs, ys)
 
 
 def plot_histogram(xs, nbins, title=None):
@@ -24,11 +31,13 @@ def plot_histogram(xs, nbins, title=None):
     plt.show()
 
 
-def plot(X_random, nsamples=1000, title=None):
+def plot(X_random, nsamples=1000, f=None, title=None):
     """Plot a histogram of the distribution X_random.
 
     X_random -- random number generator.
     nsamples -- number of samples (optional)
+    f -- PDF (optional)
+    title -- title (optional)
     """
     xs = [X_random() for _ in range(nsamples)]
     n = len(xs)
@@ -36,8 +45,13 @@ def plot(X_random, nsamples=1000, title=None):
     nbins = (max(xs) - min(xs)) / h
     nbins = int(nbins)
 
-    if len(set(xs)) < 20:
-        nbins = len(set(xs))
+    xs_set = set(xs)
+    if len(xs_set) < 20:
+        nbins = len(xs_set)
+
+    if f is not None:
+        plotcurve(f, len(xs_set), min(xs_set), max(xs_set))
+
     plot_histogram(xs, nbins, title)
 
 
