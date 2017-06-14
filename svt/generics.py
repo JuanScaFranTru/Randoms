477c2d907003e1a950ee002ebd41277e59830526
Francisco Trucco <franciscoctrucco@gmail.com>
2017-06-14 12:20:04 -0300

    Changes

--- svt/generics.py
+++ svt/generics.py
@@ -1,12 +1,21 @@
 from collections import Counter as Freq
 from collections import defaultdict
 from scipy.stats import chi2
-from random import random
 
 
 def is_rejected(pvalue, alpha):
     """Is the hypothesis rejected given alpha and pvalue?"""
     return pvalue <= alpha
+
+
+def chi2_t(sample, p):
+    a, b = min(p), max(p)
+    p = defaultdict(float, p)
+    freqs = Freq(sample)
+
+    n = sum(freqs.values())
+    t = sum([(freqs[i] - n * p[i]) ** 2 / (n * p[i]) for i in range(a, b + 1)])
+    return t
 
 
 def chi2_test(sample, p, m=0):
@@ -17,52 +26,24 @@ def chi2_test(sample, p, m=0):
     :param m: number of unknown parameters
 
     :type p: dict(number, float)
-    :type sample: list(number)
+    :type sample: list(number) or dictionary of frequencies
     :return: t, pvalue
 
-    Example:
+    Examples:
     >>> p = {0: 0.1, 1: 0.2, 2: 0.3, 3: 0.4}
     >>> sample = [0, 1, 1, 2, 2, 2, 3, 3, 3, 3]
     >>> t, pvalue = chi2_test(p, sample)
 
+    >>> def estimate(sample):
+    >>>    lam = 2.9
+    >>>    n = len(sample)
+    >>>    p = {i: exp(-lam) * lam ** i / factorial(i) for i in range(n - 1)}
+    >>>    p[n - 1] = 1 - sum(p.values())
+    >>>    return p
+    >>> sample = {0: 6, 1: 2, 2: 1, 3: 9, 4: 7, 5: 5}
+    >>> t, pvalue = chi2_test(sample, estimate(sample), 1)
     """
-    a, b = min(p), max(p)
-    k = b - a + 1
-    n = len(sample)
-
-    p = defaultdict(float, p)
-    freqs = Freq(sample)
-
-    t = sum([(freqs[i] - n * p[i]) ** 2 / (n * p[i]) for i in range(a, b + 1)])
+    k = max(p) - min(p) + 1
+    t = chi2_t(sample, p)
     pvalue = 1 - chi2.cdf(t, k - 1 - m)
-
     return t, pvalue
-
-
-def kolmogorov_test(sample, F, niter):
-    """Return the kolmogorov-Smirnov statistic, its pvalue and its dis.
-
-    Example:
-    >>> def F(x): return 1 - exp(- x / 100)
-    >>> sample = [66, 72, 81, 94, 112, 116, 124, 140, 145, 155]
-    >>> d, pvalue, dis = kolmorogorov_test(sample, F, 500)
-
-    """
-    sample.sort()
-    n = len(sample)
-    accum = [F(s) for s in sample]
-    diff1 = [(j + 1) / n - a for j, a in enumerate(accum)]
-    diff2 = [a - j / n for j, a in enumerate(accum)]
-    d = max(diff1 + diff2)
-
-    pvalue = 0
-    dis = [None] * niter
-    for i in range(niter):
-        uniforms = sorted([random() for _ in range(n)])
-        uni_diff1 = [(j + 1) / n - u for j, u in enumerate(uniforms)]
-        uni_diff2 = [u - j / n for j, u in enumerate(uniforms)]
-        dis[i] = max(uni_diff1 + uni_diff2)
-        if dis[i] > d:
-            pvalue += 1
-
-    return d, pvalue / niter, dis
---
+++ svt/kolmogorov.py
@@ -0,0 +1,30 @@
+from random import random
+
+
+def kolmogorov_test(sample, F, niter):
+    """Return the kolmogorov-Smirnov statistic, its pvalue and its dis.
+
+    Example:
+    >>> def F(x): return 1 - exp(- x / 100)
+    >>> sample = [66, 72, 81, 94, 112, 116, 124, 140, 145, 155]
+    >>> d, pvalue, dis = kolmorogorov_test(sample, F, 500)
+
+    """
+    sample.sort()
+    n = len(sample)
+    accum = [F(s) for s in sample]
+    diff1 = [(j + 1) / n - a for j, a in enumerate(accum)]
+    diff2 = [a - j / n for j, a in enumerate(accum)]
+    d = max(diff1 + diff2)
+
+    pvalue = 0
+    dis = [None] * niter
+    for i in range(niter):
+        uniforms = sorted([random() for _ in range(n)])
+        uni_diff1 = [(j + 1) / n - u for j, u in enumerate(uniforms)]
+        uni_diff2 = [u - j / n for j, u in enumerate(uniforms)]
+        dis[i] = max(uni_diff1 + uni_diff2)
+        if dis[i] > d:
+            pvalue += 1
+
+    return d, pvalue / niter, dis
