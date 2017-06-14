1d7a9d81a001db4b05f2a136f060e6f57a3dcd05
juan_sca <juansca1229@gmail.com>
2017-06-14 09:59:33 -0300

    kolmogorov-Smirnov statistic

--- svt/generics.py
+++ svt/generics.py
@@ -1,6 +1,7 @@
 from collections import Counter as Freq
 from collections import defaultdict
 from scipy.stats import chi2
+from random import random
 
 
 def is_rejected(pvalue, alpha):
@@ -8,7 +9,7 @@ def is_rejected(pvalue, alpha):
     return pvalue <= alpha
 
 
-def chi2_test(p, sample):
+def chi2_test(sample, p):
     """Return the chi squared statistic and its pvalue
 
     :param p: hypothesized probability distribution
@@ -35,3 +36,32 @@ def chi2_test(p, sample):
     pvalue = 1 - chi2.cdf(t, k)
 
     return t, pvalue
+
+
+def kolmogorov_test(sample, F, niter):
+    """Return the kolmogorov-Smirnov statistic, its pvalue and its dis
+
+    Example:
+    >>> def F(x): return 1 - exp(- x / 100)
+    >>> sample = [66, 72, 81, 94, 112, 116, 124, 140, 145, 155]
+    >>> d, pvalue, dis = kolmorogorov_test(sample, F, 500)
+
+    """
+    sample.sort()
+    n = len(sample)
+    accum = [F(s) for s in sample]
+    diff1 = [(j + 1) / n - a for j, a in enumerate(accum)]
+    diff2 = [a - j / n for j, a in enumerate(accum)]
+    d = max(diff1 + diff2)
+
+    pvalue = 0
+    dis = [None] * niter
+    for i in range(niter):
+        uniforms = sorted([random() for _ in range(n)])
+        uni_diff1 = [(j + 1) / n - u for j, u in enumerate(uniforms)]
+        uni_diff2 = [u - j / n for j, u in enumerate(uniforms)]
+        dis[i] = max(uni_diff1 + uni_diff2)
+        if dis[i] > d:
+            pvalue += 1
+
+    return d, pvalue / niter, dis
