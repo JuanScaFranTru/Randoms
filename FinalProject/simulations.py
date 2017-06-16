560ddfcee213bac78b4f749c335fbc85f7d2e351
juan_sca <juansca1229@gmail.com>
2017-06-16 14:41:12 -0300

    Initial commit to ex1

--- FinalProject/simulations.py
+++ FinalProject/simulations.py
@@ -1,9 +1,17 @@
+from random import random
+from math import log
+
+
+def exponential(lambda_):
+    """Exponential distribution."""
+    U = random()
+    return (-log(U)) / lambda_
 
 
 class Simulation(object):
     def __init__(self):
         name = self.__class__.__name__
-        msg = 'Exercise {}'.format(name)
+        msg = 'Simulation {}'.format(name)
         print()
         print(msg)
         print(len(msg) * '-')
@@ -13,7 +21,17 @@ class Simulation(object):
 
 
 class One(Simulation):
+    """Inicializar el programa con un sorteo de los tiempos de fallos de cada
+    una las máquinas en uso, y ejecutarlo para estimar el tiempo medio de falla
+    del sistema y su correspondiente desviación estándar.
 
+    Expresar todos los tiempos usando como unidad el mes. Utilizar:
+    N = 5, S = 2, y suponer que el tiempo medio de fallo de una máquina es
+    Tf = 1 mes y que el tiempo medio medio de reparación de una máquina es
+    Tr = 1/8 mes.
+
+    Se realizan 10000 simulaciones de tiempos de fallo.
+    """
     def __init__(self, N, S, Tf, Tr, nsim=1):
         """
         :param N: Number of in service machines
@@ -26,6 +44,7 @@ class One(Simulation):
             mean_t, dev_t = self.run(N, S, Tf, Tr)
         self.mean_t = mean_t
         self.dev_t = dev_t
+
 
 
 if __name__ == '__main__':
